44da6be6d053ee77c0fdb61e67837888d9e8bea3
kejyang <k.yang256@gmail.com>
2025-02-07 07:16:30 -0800

    Organized and refactored tab bar files to be cleaner

---
+++ Habito/Views/TabBarViews/TabBarItemsView.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarItems.swift
+//  Habito
+//
+//  Created by Kenneth Yang on 2/7/25.
+//
+
+
+import SwiftUI
+
+struct TabBarItemsView: View {
+    @Binding var selectedTab: Int
+
+    var body: some View {
+        HStack(alignment: .bottom) {
+            Spacer().frame(width: 5)
+            // First Tab Button
+            TabBarItemView(iconName: "house.fill", iconNameNotSelected: "house", label: "Home", index: 0, selectedTab: $selectedTab)
+
+            // Second Tab Button
+            TabBarItemView(iconName: "book.fill", iconNameNotSelected: "book", label: "Guides", index: 1, selectedTab: $selectedTab)
+
+            Spacer().frame(width: 110)
+
+            // Third Tab Button
+            TabBarItemView(iconName: "clipboard.fill", iconNameNotSelected: "clipboard", label: "Data", index: 2, selectedTab: $selectedTab)
+
+            Spacer()
+
+            // Fourth Tab Button
+            TabBarItemView(iconName: "person.fill", iconNameNotSelected: "person", label: "Profile", index: 3, selectedTab: $selectedTab)
+        }
+        .frame(height: 90)
+    }
+}
--- Habito/Views/TabBarViews/TestTabBarView.swift
+++ Habito/Views/TabBarViews/TestTabBarView.swift
@@ -31,7 +31,7 @@ struct TestTabBarView: View {
                                     .offset(x: 0, y: -45)
                             )
                         
-                        TabBarItems(selectedTab: $selectedTab)
+                        TabBarItemsView(selectedTab: $selectedTab)
                     }
                 }
             }
@@ -40,62 +40,9 @@ struct TestTabBarView: View {
     }
 }
 
-struct TabBarShape: Shape {
-    private enum Constants {
-        static let cornerRadius: CGFloat = 20
-        static let buttonRadius: CGFloat = 45
-        static let buttonPadding: CGFloat = 5
-    }
 
-    func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath()
 
-        path.move(to: .init(x: 0, y: rect.height))
-        path.addLine(to: .init(x: 0, y: rect.height - Constants.cornerRadius))
-        path.addArc(withCenter: .init(x: 0, y: Constants.cornerRadius), radius: Constants.cornerRadius, startAngle: CGFloat.pi, endAngle: -CGFloat.pi/2, clockwise: true)
 
-        let lineEnd = rect.width/2 - 2 * Constants.buttonPadding - Constants.buttonRadius
-        path.addLine(to: .init(x: lineEnd, y: 0))
-        path.addArc(withCenter: .init(x: lineEnd, y: Constants.buttonPadding), radius: Constants.buttonPadding, startAngle: -CGFloat.pi/2, endAngle: 0, clockwise: true)
-        path.addArc(withCenter: .init(x: rect.width/2, y: Constants.buttonPadding), radius: Constants.buttonPadding + Constants.buttonRadius, startAngle: CGFloat.pi, endAngle: 0, clockwise: false)
-
-        let lineStart = rect.width/2 + 2 * Constants.buttonPadding + Constants.buttonRadius
-        path.addArc(withCenter: .init(x: lineStart, y: Constants.buttonPadding), radius: Constants.buttonPadding, startAngle: CGFloat.pi, endAngle: -CGFloat.pi/2, clockwise: true)
-
-        path.addLine(to: .init(x: rect.width - Constants.cornerRadius, y: 0))
-        path.addArc(withCenter: .init(x: rect.width, y: Constants.cornerRadius), radius: Constants.cornerRadius, startAngle: -CGFloat.pi/2, endAngle: 0, clockwise: true)
-        path.addLine(to: .init(x: rect.width, y: rect.height))
-        path.close()
-
-        return Path(path.cgPath)
-    }
-}
-
-struct TabBarItems: View {
-    @Binding var selectedTab: Int
-
-    var body: some View {
-        HStack(alignment: .bottom) {
-            Spacer().frame(width: 5)
-            // First Tab Button
-            TabBarItemView(iconName: "house.fill", iconNameNotSelected: "house", label: "Home", index: 0, selectedTab: $selectedTab)
-
-            // Second Tab Button
-            TabBarItemView(iconName: "book.fill", iconNameNotSelected: "book", label: "Guides", index: 1, selectedTab: $selectedTab)
-
-            Spacer().frame(width: 110)
-
-            // Third Tab Button
-            TabBarItemView(iconName: "clipboard.fill", iconNameNotSelected: "clipboard", label: "Data", index: 2, selectedTab: $selectedTab)
-
-            Spacer()
-
-            // Fourth Tab Button
-            TabBarItemView(iconName: "person.fill", iconNameNotSelected: "person", label: "Profile", index: 3, selectedTab: $selectedTab)
-        }
-        .frame(height: 90)
-    }
-}
 
 
 #Preview {
