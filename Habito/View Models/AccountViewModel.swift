2a7641e9085dc1986f3fda5d479af0a097b6150b
Edward <edwardnguyen248@gmail.com>
2025-02-12 21:06:48 -0500

    Sign up validation

--- Habito/View Models/AccountViewModel.swift
+++ Habito/View Models/AccountViewModel.swift
@@ -9,10 +9,24 @@ import Foundation
 
 class AccountViewModel: ObservableObject {
     @Published var account : AccountModel?
+    @Published var isLoggedIn = false
+    
+    enum ValidationResponse {
+        case valid
+        case emailEmpty
+        case emailContainsSpace
+        case invalidEmailFormat
+        case emailInUse
+        case passwordNotMatching
+        case passwordNotLong
+        case passwordNoSpecialCharacter
+        case passwordNoNumber
+        case passwordNoCapital
+    }
     
     init() {
-        //AccountDAO.shared.createDatabase()
-        //AccountDAO.shared.createAccountTable()
+        AccountDAO.shared.createDatabase()
+        AccountDAO.shared.createAccountTable()
     }
     
     func validateLogin(email: String, password: String) -> Bool {
@@ -23,18 +37,46 @@ class AccountViewModel: ObservableObject {
     }
     
     func validateSignup(username: String, email: String, password: String, confirmPassword: String) -> Bool {
-        if SignupValidator.shared.validate(username: username, email: email, password: password, confirmPassword: confirmPassword) == SignupValidator.ValidationResponse.valid {
+        if SignupValidator.shared.validate(username: username, email: email, password: password, confirmPassword: confirmPassword).isEmpty {
             return true
         }
         return false
     }
     
-    func signupAccount(username: String, email: String, password: String) {
+    func signupAccount(username: String, email: String, password: String) -> AccountModel? {
         AccountDAO.shared.insertAccount(username: username as NSString, email: email.lowercased() as NSString, password: password as NSString)
-        print(AccountDAO.shared.fetchAccountByEmail(email: email.lowercased() as NSString))
+        return AccountDAO.shared.fetchAccountByEmail(email: email.lowercased() as NSString)
     }
     
-    func setSignedInAccount() {
-        //account = AccountModel()
+    func attemptSignup(username: String, email: String, password: String, confirmPassword: String) -> Dictionary<ValidationResponse,Bool> {
+        let validationResponses = SignupValidator.shared.validate(username: username, email: email, password: password, confirmPassword: confirmPassword)
+        
+        var responseDictionary = Dictionary<ValidationResponse,Bool>()
+        responseDictionary[ValidationResponse.valid] = validationResponses.isEmpty
+        responseDictionary[ValidationResponse.emailEmpty] = validationResponses.contains(SignupValidator.ValidationResponse.emailEmpty)
+        responseDictionary[ValidationResponse.emailContainsSpace] = validationResponses.contains(SignupValidator.ValidationResponse.emailContainsSpace)
+        responseDictionary[ValidationResponse.invalidEmailFormat] = validationResponses.contains(SignupValidator.ValidationResponse.invalidEmailFormat)
+        responseDictionary[ValidationResponse.emailInUse] = validationResponses.contains(SignupValidator.ValidationResponse.emailInUse)
+        responseDictionary[ValidationResponse.passwordNotMatching] = validationResponses.contains(SignupValidator.ValidationResponse.passwordNotMatching)
+        responseDictionary[ValidationResponse.passwordNotLong] = validationResponses.contains(SignupValidator.ValidationResponse.passwordNotLong)
+        responseDictionary[ValidationResponse.passwordNoSpecialCharacter] = validationResponses.contains(SignupValidator.ValidationResponse.passwordNoSpecialCharacter)
+        responseDictionary[ValidationResponse.passwordNoNumber] = validationResponses.contains(SignupValidator.ValidationResponse.passwordNoNumber)
+        responseDictionary[ValidationResponse.passwordNoCapital] = validationResponses.contains(SignupValidator.ValidationResponse.passwordNoCapital)
+        
+        guard validationResponses.isEmpty else {
+            return responseDictionary
+        }
+        
+        if let account = signupAccount(username: username, email: email, password: password) {
+            setSignedInAccount(accountModel: account)
+        } else {
+            responseDictionary[ValidationResponse.valid] = false
+        }
+        
+        return responseDictionary
+    }
+    
+    func setSignedInAccount(accountModel: AccountModel) {
+        account = accountModel
     }
 }
--- Habito/Views/AuthenticationViews/SignUpView.swift
+++ Habito/Views/AuthenticationViews/SignUpView.swift
@@ -16,62 +16,144 @@ struct SignUpView: View {
     @State var confirmPasswordTextFieldText: String = ""
     @State var isAgreeTerms: Bool = false
     
+    @State var isEmailEmpty = false
+    @State var isEmailSpace = false
+    @State var isEmailWrongFormat = false
+    @State var isEmailUsed = false
+    
+    @State var isPasswordShort = false
+    @State var isPasswordNoNumber = false
+    @State var isPasswordNoCapital = false
+    @State var isPasswordNoSpecial = false
+    
+    @State var isPasswordNoMatch = false
+    
+    @State var isSignupSuccess: Bool? = false
+    
     var body: some View {
-        VStack {
-            
-            Text("Create Account")
-                .font(.headline)
-                .padding()
-            
-            Text("Let's create an account for you!")
-                .foregroundColor(Color.brandPrimary)
-                .font(.subheadline.bold())
-                .padding(.bottom)
-            
-            TextFieldGeneralView(heading: "Username", textFieldText: $usernameTextFieldText)
-                .padding(.bottom)
-            
-            TextFieldGeneralView(heading: "E-mail", textFieldText: $emailTextFieldText)
-                .padding(.bottom)
-            
-            SecureFieldGeneralView(heading: "Password", textFieldText: $passwordTextFieldText)
-                .padding(.bottom)
-            
-            SecureFieldGeneralView(heading: "Confirm Password", textFieldText: $confirmPasswordTextFieldText)
-            
-            
-            HStack {
-                Toggle("", isOn: $isAgreeTerms)
-                    .toggleStyle(CheckboxToggleStyle())
-                    .frame(maxWidth: 40, alignment: .leading)
-                Text("I agree to the")
-                    .frame(alignment: .leading)
-                NavigationLink("terms and privacy policy", destination: ContentView())
-                    .lineLimit(1)
-                    .frame(maxWidth: .infinity, alignment: .leading)
+        if accountViewModel.isLoggedIn {
+            TabBarView()
+                .transition(.move(edge: .leading))
+        } else {
+            VStack {
+                
+                Text("Create Account")
+                    .font(.headline)
+                    .padding()
+                
+                Text("Let's create an account for you!")
                     .foregroundColor(Color.brandPrimary)
-            }
-            .padding()
-            
-            
-            //NavigationLink(destination: HomeView(), label: {
+                    .font(.subheadline.bold())
+                    .padding(.bottom)
+                
+                TextFieldGeneralView(heading: "Username", textFieldText: $usernameTextFieldText)
+                    .padding(.bottom)
+                
+                TextFieldGeneralView(heading: "E-mail", textFieldText: $emailTextFieldText, isRed: isEmailUsed || isEmailEmpty || isEmailSpace || isEmailWrongFormat)
+                    .padding(.bottom)
+                
+                VStack (alignment: .leading) {
+                    if isEmailUsed {
+                        Text("An account already exists with this email")
+                    }
+                    if isEmailWrongFormat {
+                        Text(verbatim: "Email must be in the format abc@xyz.com")
+                    }
+                    if isEmailSpace {
+                        Text("Email cannot contain spaces")
+                    }
+                    if isEmailEmpty {
+                        Text("Email cannot be empty")
+                    }
+                }
+                .frame(width: SizeStandards.widthGeneral, alignment: .leading)
+                .foregroundColor(.red)
+                .font(.subheadline)
+                
+                
+                SecureFieldGeneralView(heading: "Password", textFieldText: $passwordTextFieldText, isRed: isPasswordShort || isPasswordNoNumber || isPasswordNoCapital || isPasswordNoSpecial)
+                    .padding(.bottom)
+                
+                VStack (alignment: .leading) {
+                    if isPasswordShort {
+                        Text("Password must be atleast 6 characters")
+                    }
+                    if isPasswordNoSpecial {
+                        Text("Password must contain a special character")
+                    }
+                    if isPasswordNoNumber {
+                        Text("Password must contain a number")
+                    }
+                    if isPasswordNoCapital {
+                        Text("Password must contain a capital")
+                    }
+                }
+                .frame(width: SizeStandards.widthGeneral, alignment: .leading)
+                .foregroundColor(.red)
+                .font(.subheadline)
+                
+                SecureFieldGeneralView(heading: "Confirm Password", textFieldText: $confirmPasswordTextFieldText, isRed: isPasswordNoMatch)
+                
+                VStack (alignment: .leading) {
+                    if isPasswordNoMatch {
+                        Text("Passwords do not match")
+                    }
+                }
+                .frame(width: SizeStandards.widthGeneral, alignment: .leading)
+                .foregroundColor(.red)
+                .font(.subheadline)
+                /*HStack {
+                 Toggle("", isOn: $isAgreeTerms)
+                 .toggleStyle(CheckboxToggleStyle())
+                 .frame(maxWidth: 40, alignment: .leading)
+                 Text("I agree to the")
+                 .frame(alignment: .leading)
+                 NavigationLink("terms and privacy policy", destination: ContentView())
+                 .lineLimit(1)
+                 .frame(maxWidth: .infinity, alignment: .leading)
+                 .foregroundColor(Color.brandPrimary)
+                 }*/
+                .padding()
+                
+                
+                //NavigationLink(destination: HabitView(), tag: true, selection: $isSignupSuccess, label: {
                 Button("Sign Up") {
-                    //signup()
+                    if signup() {
+                        accountViewModel.isLoggedIn = true
+                    }
                 }
                 .frame(width: SizeStandards.widthGeneral, height: SizeStandards.actionButtonHeight, alignment: .center)
                 .modifier(ActionButtonModifier())
-            //})
-            
+                //})
+                
+            }
         }
     }
     
-    func signup() {
-        accountViewModel.signupAccount(username: usernameTextFieldText, email: emailTextFieldText, password: passwordTextFieldText)
-        presentationMode.wrappedValue.dismiss()
+    func signup() -> Bool {
+        //let responses = accountViewModel.signupAccount(username: usernameTextFieldText, email: emailTextFieldText, password: passwordTextFieldText)
+        let responses = accountViewModel.attemptSignup(username: usernameTextFieldText, email: emailTextFieldText, password: passwordTextFieldText, confirmPassword: confirmPasswordTextFieldText)
+        
+        isEmailEmpty = responses[AccountViewModel.ValidationResponse.emailEmpty] ?? false
+        isEmailSpace = responses[AccountViewModel.ValidationResponse.emailContainsSpace] ?? false
+        isEmailWrongFormat = responses[AccountViewModel.ValidationResponse.invalidEmailFormat] ?? false
+        isEmailUsed = responses[AccountViewModel.ValidationResponse.emailInUse] ?? false
+        isPasswordShort = responses[AccountViewModel.ValidationResponse.passwordNotLong] ?? false
+        isPasswordNoNumber = responses[AccountViewModel.ValidationResponse.passwordNoNumber] ?? false
+        isPasswordNoCapital = responses[AccountViewModel.ValidationResponse.passwordNoCapital] ?? false
+        isPasswordNoSpecial = responses[AccountViewModel.ValidationResponse.passwordNoSpecialCharacter] ?? false
+        
+        isPasswordNoMatch = responses[AccountViewModel.ValidationResponse.passwordNotMatching] ?? false
+        
+        return responses[AccountViewModel.ValidationResponse.valid] ?? false
+        //presentationMode.
     }
     
 }
 
 #Preview {
-    SignUpView()
+    NavigationView {
+        SignUpView()
+    }
+    .environmentObject(AccountViewModel())
 }
