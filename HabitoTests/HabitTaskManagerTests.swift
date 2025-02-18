b4e1e6501a77f06a5d25696548554d5e8c588374
Edward <edwardnguyen248@gmail.com>
2025-02-18 15:25:16 -0500

    HabitTaskManager tests

--- HabitoTests/HabitTaskManagerTests.swift
+++ HabitoTests/HabitTaskManagerTests.swift
@@ -6,30 +6,117 @@
 //
 
 import XCTest
+@testable import Habito
 
 final class HabitTaskManagerTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    var dbManager: DBManager!
+    let accountId = 1
+    
+    override func setUp() {
+        super.setUp()
+        dbManager = DBManager()
+        dbManager.createDatabase(inMemory: true) // Use in-memory database for testing
+        dbManager.createAccountTable()
+        dbManager.createHabitTable() // Create the necessary table for testing
+        dbManager.createHabitDailyTaskTable()
+        
+        // Setup environment
+        dbManager.insertAccount(username: "XCTest", email: "XCTest7547457@xyz.com" as NSString, password: "Abc123!")
+        
+        let title = "Test Habit"
+        let habitDetails = "Test Details"
+        let activityType = ActivityType.drinkingWater.rawValue
+        let timeOfTheDay = TimeOfDay.Afternoon.rawValue
+        let accountId = 1
+        
+        // When
+        dbManager.insertHabit(title: title, habitDetails: habitDetails, activityType: activityType, timeOfTheDay: timeOfTheDay, accountId: accountId)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        dbManager = nil
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testGetTimeOfDayString_Morning() {
+        let time = HabitTaskManager.shared.getTimeOfDayFromString(time: TimeOfDay.Morning.rawValue)
+        XCTAssertEqual(TimeOfDay.Morning, time)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testGetTimeOfDayString_Evening() {
+        let time = HabitTaskManager.shared.getTimeOfDayFromString(time: TimeOfDay.Evening.rawValue)
+        XCTAssertEqual(TimeOfDay.Evening, time)
     }
-
+    
+    func testGetTimeOfDayString_Afternoon() {
+        let time = HabitTaskManager.shared.getTimeOfDayFromString(time: TimeOfDay.Afternoon.rawValue)
+        XCTAssertEqual(TimeOfDay.Afternoon, time)
+    }
+    
+    func testGetTimeOfDayString_Night() {
+        let time = HabitTaskManager.shared.getTimeOfDayFromString(time: TimeOfDay.Night.rawValue)
+        XCTAssertEqual(TimeOfDay.Night, time)
+    }
+    
+    func testGetTimeOfDayString_Anytime() {
+        let time = HabitTaskManager.shared.getTimeOfDayFromString(time: TimeOfDay.Anytime.rawValue)
+        XCTAssertEqual(TimeOfDay.Anytime, time)
+    }
+    
+    func testGetTimeOfDayString_Morning_RandomString() {
+        let time = HabitTaskManager.shared.getTimeOfDayFromString(time: "efsef")
+        XCTAssertEqual(TimeOfDay.Morning, time)
+    }
+    
+    func testGetTaskProgressDescription_ReturnsString_Sleep() {
+        let stringVal = HabitTaskManager.shared.getTaskProgressDescription(progress: 0, activityType: ActivityType.sleep.rawValue)
+        XCTAssertEqual("0/8 Hours", stringVal)
+    }
+    
+    func testGetTaskProgressDescription_ReturnsString_Water() {
+        let stringVal = HabitTaskManager.shared.getTaskProgressDescription(progress: 0, activityType: ActivityType.drinkingWater.rawValue)
+        XCTAssertEqual("0/8 Glasses", stringVal)
+    }
+    
+    func testGetTaskProgressDescription_ReturnsString_Biking() {
+        let stringVal = HabitTaskManager.shared.getTaskProgressDescription(progress: 0, activityType: ActivityType.biking.rawValue)
+        XCTAssertEqual("0/30 Minutes", stringVal)
+    }
+    
+    
+    func testGetTaskProgressDescription_ReturnsString_Running() {
+        let stringVal = HabitTaskManager.shared.getTaskProgressDescription(progress: 0, activityType: ActivityType.running.rawValue)
+        XCTAssertEqual("0K/10K Steps", stringVal)
+    }
+    
+    func testGetTaskProgressDescription_ReturnsEmpty_RandomString() {
+        let stringVal = HabitTaskManager.shared.getTaskProgressDescription(progress: 0, activityType: "seffse")
+        XCTAssertEqual("", stringVal)
+    }
+    
+    func testGetActivityTypeMaxValue_ReturnsInt_Sleep() {
+        let val = HabitTaskManager.shared.getActivityTypeMaxValue(activityType: ActivityType.sleep.rawValue)
+        XCTAssertEqual(8, val)
+    }
+    
+    func testGetActivityTypeMaxValue_ReturnsInt_Water() {
+        let val = HabitTaskManager.shared.getActivityTypeMaxValue(activityType: ActivityType.drinkingWater.rawValue)
+        XCTAssertEqual(8, val)
+    }
+    
+    func testGetActivityTypeMaxValue_ReturnsInt_Biking() {
+        let val = HabitTaskManager.shared.getActivityTypeMaxValue(activityType: ActivityType.biking.rawValue)
+        XCTAssertEqual(30, val)
+    }
+    
+    func testGetActivityTypeMaxValue_ReturnsInt_Running() {
+        let val = HabitTaskManager.shared.getActivityTypeMaxValue(activityType: ActivityType.running.rawValue)
+        XCTAssertEqual(10, val)
+    }
+    
+    func testGetActivityTypeMaxValue_ReturnsInt_RandomString() {
+        let val = HabitTaskManager.shared.getActivityTypeMaxValue(activityType: "esfsefsef")
+        XCTAssertNil(val)
+    }
 }
